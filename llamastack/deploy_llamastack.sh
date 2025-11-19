7f84346b5d9a1cdde6641201956d18ef762907b2
Hadar Cohen <hacohen@redhat.com>
2025-11-19 13:44:45 +0200

    deploying llamastack on openshift

--- llamastack/deploy_llamastack.sh
+++ llamastack/deploy_llamastack.sh
@@ -5,7 +5,7 @@ oc apply -f service.yaml
 oc apply -f deployment.yaml
 oc wait --for=condition=available deployment/llamastack --timeout=300s
 sleep 3s
-oc apply -f route.yaml
+# oc apply -f route.yaml
 
 oc get pods | grep llamastack
-oc get route | grep llamastack
+# oc get route | grep llamastack
